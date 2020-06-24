e99a6f66b6ecfcf3d61a45f73a9f1dffc5652d0e
Giulia <giulialosito94@gmail.com>
2020-06-24 18:02:34 +0200

    esercizio post comprensione comando

--- js/script.js
+++ js/script.js
@@ -1,4 +1,26 @@
 
 var cognomi = ["Rossi", "Cotti", "Grossi", "Porti"];
-cognomi.sort();
+var cognomiOrdinati;
+var cognomeUtente;
+var lista;
+var listaPrec;
+var indice;
+
 console.log(cognomi);
+
+var cognomeUtente = prompt("Inserisci il tuo cognome");
+cognomi.push("cognomeUtente");
+
+cognomiOrdinati = cognomi.sort();
+console.log(cognomiOrdinati);
+
+for (var i = 0; i < cognomiOrdinati.length; i++) {
+  lista = cognomiOrdinati[i];
+  listaPrima = document.getElementById('lista').innerHTML;
+  document.getElementById("lista").innerHTML= listaPrima + "<li>" + lista + "</li>" ;
+
+
+}
+
+indice = (cognomiOrdinati.indexOf(cognomeUtente)) + 1;
+document.getElementById("posizione").innerHTML = "Il cognome inserito è nella posizione: " + indice;
